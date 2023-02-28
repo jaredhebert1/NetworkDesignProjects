5d060d705c8d3af811e7eb5f7fae7b4877aae021
jaredhebert1 <66858948+jaredhebert1@users.noreply.github.com>
2023-02-28 17:31:07 -0500

    Update client_socket.cpp

--- Source Files/client_socket.cpp
+++ Source Files/client_socket.cpp
@@ -1,10 +1,10 @@
 /*
-	Jared Hebert and Andrew O'Brien
+	Jared Hebert
+	Andrew O'Brein
 	EECE.4830
-	Project Phase 2
-	RDT 1.0 Client
+	UDP CLient
 
-	code from https://www.binarytides.com/udp-socket-programming-in-winsock/ used
+	some code from https://www.binarytides.com/udp-socket-programming-in-winsock/ used
 */
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
@@ -12,49 +12,108 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include <iostream>
+#include<time.h>
+#include<iostream>
 #include<winsock2.h>
-#include <WS2tcpip.h>
+#include<WS2tcpip.h>
 
 
 using namespace::std;
 
-char packetData[1024];
-char inStr[] = "bmp_example.bmp";
-//char outStr[] = "bmp_out.bmp";
-FILE* fpIn;
-//FILE* fpOut;
-
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
-#define BUFLEN 1024
-#define packetDebugMode 1
+#define BUFLEN 1025 
+#define packetDebugMode 0
 
-int Make_Packet(int newFile) {
+#define corruptionPercent 0.000
+#define corruptionStrength 0.000
+
+char inStr[] = "bmp_small.bmp";
+char outStr[] = "bmp_out.bmp";
+
+int Make_Packet(FILE** fpIn, char* packetData) {
+	/*
+		updates packetData array to be the next 1KB in your defined "fpIn"
+
+		returns 1 when the end of the file has been reached
+		otherwise 0
+
+	*/
 	int freadReturn;
-	int fileEnd;
-	
-	if (newFile == 1) {
-		fpIn = fopen(inStr, "rb"); 
-		
-		if (fpIn == NULL) {
-			printf("Failed to Open file\n"); 
-			return -1;			
-		}
-	}
-	
-	freadReturn = fread(packetData, 1, 1024, fpIn); 
+	int fileEnd = 0;
+
+
+	freadReturn = fread(packetData, 1, 1024, *fpIn);
 	if (freadReturn == 0) {
 		fileEnd = 1;
 	}
-	
-	if (fileEnd == 1) {
-		fclose(fpIn); 
-	}
-	
-	return fileEnd; 
-	
+
+	return fileEnd;
 }
+
+int Make_File(FILE** fpOut, char* packetData) {
+
+	/*
+		writes the data contained in packetData to the file defined "fpOut"
+
+		returns 1 when no data was written
+		otherwise 0
+
+	*/
+
+	int fwriteReturn;
+	int fileEnd = 0;
+
+
+	fwriteReturn = fwrite(packetData, 1, 1024, *fpOut);
+	printf("fwriteReturn = %d\n", fwriteReturn);
+	if (fwriteReturn != 1024) {
+		fileEnd = 1;
+	}
+
+	return fileEnd;
+}
+
+int Corruptor_Challenge(float challenge	, float corruption_amount, char* packetData) {
+	/*
+		passes a pacekt of data through a challenge, difficulty determined by the value of challenge in a decimal representation of the percent
+		difficulty, 3 sig-figs.
+
+		corruption_ammount is the decimal representation of the ammount of corruption each corrupted packet will expirence.
+
+		if the challenge hits, the packet will be randomly disrupted. if not it will be unaffected.
+
+		returns 1 if challenge hits, otherwise 0
+	
+	*/
+
+	int random_num, challenge_result;
+
+	random_num = rand() % 1000;
+
+	if (random_num < (challenge * 1000)) {
+		// challenge hits
+
+		for (int i = 0; i < 1024; ++i) {
+			int r = rand() % 1000;
+			if (r < (corruption_amount * 1000)) {
+				packetData[i] = packetData[i] ^ 0xFF;
+			}
+		}
+
+		challenge_result = 1;
+	}
+	else {
+		//challenge misses
+
+		challenge_result = 0;
+	}
+
+	return challenge_result;
+}
+
+
+
 
 int main(int argc, char* argv[])
 {
@@ -62,15 +121,24 @@ int main(int argc, char* argv[])
 	SOCKET client;
 	sockaddr_in client_addr, si_other;
 	hostent* localHost;
+	
+	FILE* fpIn;
+	FILE* fpOut;
 
 	char buf[BUFLEN];
-	//char message[BUFLEN] = "hello";	//message to send
+	char message[BUFLEN] = "file_send_done";
 
 	int slen;
 
 	slen = sizeof(si_other);
 
 	char* localIP;
+
+	int imgSize;	//in kB
+	char packetData[BUFLEN];
+
+	srand(time(NULL));		//Initialize random numbers
+
 
 
 	//Init winsock library
@@ -98,45 +166,110 @@ int main(int argc, char* argv[])
 	si_other.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	si_other.sin_port = htons(8888);
 
-	if (packetDebugMode == 1) {
-		
-		int fileEnd = 0;
-		int newFile = 1;
-		
-			
-		// Start Comunication 
-		while (fileEnd == 0) {
-			
+	//Open files
 
-			fileEnd = Make_Packet(newFile);
-			if (newFile == 1) {
-				newFile = 0;
+	fpIn = fopen(inStr, "rb");
+	if (fpIn == NULL) {
+		printf("In file failed to open :( \n");
+		return 1;
+	}
+
+	if (packetDebugMode == 1) {
+
+		fpOut = fopen(outStr, "wb");
+		if (fpOut == NULL) {
+			printf("Out file failed to open :( \n");
+			return 1;
+		}
+	}
+
+	// The function below ingests a file and reproduces it 1KB at a time, only run when the global packetDebugMode is set to 1.
+
+	int fileEnd = 0;
+
+	if (packetDebugMode == 1) {
+
+
+		int fileEnd = 0;
+		int fileEndOut = 0;
+		int packetCorrupted;
+
+		while (fileEnd == 0) {
+
+			fileEnd = Make_Packet(&fpIn, packetData);
+
+			packetCorrupted = Corruptor_Challenge(corruptionPercent, corruptionStrength, packetData);
+
+			if (packetCorrupted) {
+				printf("Packet Corrupted\n");
 			}
-			
-			if (fileEnd == -1) {
-				printf("File did not open\n"); 
-				return 1;
-			}
-			
-			if (sendto(client, packetData, strlen(packetData), 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR){
+			//printf("%s\n", packetData);
+			fileEndOut = Make_File(&fpOut, packetData);
+
+			//printf("fileEndIn = %d fileEndOut = %d\n", fileEnd, fileEndOut);
+
+		}
+
+		if (fileEnd == 1) {
+			fclose(fpIn);
+			fclose(fpOut);
+		}
+
+		return 0;
+	}
+
+	
+
+	//start communication
+
+	while (fileEnd == 0)
+	{
+
+		fileEnd = Make_Packet(&fpIn, packetData);
+
+		if (fileEnd == 0) {
+			//send the message
+			if (sendto(client, packetData, strlen(packetData), 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
+			{
 				printf("sendto() failed with error code : %d", WSAGetLastError());
 				exit(EXIT_FAILURE);
 			}
-
-			//receive a reply and print it
-			//clear the buffer by filling null, it might have previously received data
-			memset(buf, '\0', BUFLEN);
-			//try to receive some data, this is a blocking call
-			if (recvfrom(client, buf, BUFLEN, 0, (struct sockaddr*)&si_other, &slen) == SOCKET_ERROR){
-				printf("recvfrom() failed with error code : %d", WSAGetLastError());
+		}
+		else {
+			//send the message
+			if (sendto(client, message, strlen(message), 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
+			{
+				printf("sendto() failed with error code : %d", WSAGetLastError());
 				exit(EXIT_FAILURE);
 			}
-			puts(buf);
-			Sleep(500);
-				
-		}	
+		}
+
+		//receive a reply and print it
+		//clear the buffer by filling null, it might have previously received data
+		memset(buf, '\0', BUFLEN);
+		//try to receive some data, this is a blocking call
+		if (recvfrom(client, buf, BUFLEN, 0, (struct sockaddr*)&si_other, &slen) == SOCKET_ERROR)
+		{
+			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		puts(buf);
+
+		//delay 500ms to artificially slow program down
+		Sleep(100);
+
+		if (fileEnd == 1) {
+			fclose(fpIn);
+		}
 	}
 
+	//close socket
+	closesocket(client);
+	WSACleanup();
+
+	return 0;
+}
 	//close socket
 	closesocket(client);
 	WSACleanup();
